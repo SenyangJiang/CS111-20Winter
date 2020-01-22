272a2f27bce996cf68986608e2195109d91afe4d
Senyang Jiang <senyangjiang@yahoo.com>
2020-01-22 11:38:46 -0800

    Add files via upload

--- proj1a/lab1a.c
+++ proj1a/lab1a.c
@@ -7,6 +7,8 @@
 #include <poll.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
 
 #define BUFSIZE 256
 struct termios saved_attr;
@@ -17,8 +19,6 @@ void set_input_mode(void) {
   tattr.c_iflag = ISTRIP;
   tattr.c_oflag = 0;
   tattr.c_lflag = 0;
-  //tattr.c_cc[VMIN] = 1;
-  //tattr.c_cc[VTIME] = 0;
   tcsetattr(STDIN_FILENO, TCSANOW, &tattr);
 }
 
@@ -27,8 +27,6 @@ void reset_input_mode(void) {
 }
 
 int main(int argc, char** argv) {
-
-  set_input_mode();
   
   int opt;
   struct option long_options[2] = {{"shell", no_argument, NULL, 's'},
@@ -36,19 +34,28 @@ int main(int argc, char** argv) {
   int shell = 0;
 
   while(1) {
-    opt = getopt_long(argc, argv, "", long_options, NULL);
+    opt = getopt_long(argc, argv, ":", long_options, NULL);
     if(opt == -1)
       break;
     switch(opt) {
     case 's':
       shell = 1;
       break;
-    default:
+    case '?':
+      fprintf(stderr, "Unrecognized argument: %s\n",  argv[optind-1]);
       exit(1);
       break;
     }
   }
 
+  if(optind != argc)
+  {
+    fprintf(stderr, "Unrecognized argument: %s\n", argv[optind]);
+    exit(1);
+  }
+  
+  set_input_mode();
+  
   char buffer[BUFSIZE];
   int n;
   
@@ -56,12 +63,18 @@ int main(int argc, char** argv) {
     int pipefd_in[2]; // to shell
     int pipefd_out[2]; // to terminal process
     if(pipe(pipefd_in) == -1) {
+      fprintf(stderr, strerror(errno));
       exit(1);
     }
     if(pipe(pipefd_out) == -1) {
+      fprintf(stderr, strerror(errno));
       exit(1);
     }
     pid_t c_pid = fork();
+    if(c_pid == -1) {
+      fprintf(stderr, strerror(errno));
+      exit(1);
+    }
     if(c_pid == 0) {
       close(pipefd_in[1]);
       close(pipefd_out[0]);
@@ -76,7 +89,10 @@ int main(int argc, char** argv) {
       dup(pipefd_out[1]);
       close(pipefd_out[1]);
       
-      execl("/bin/bash", "", (char*)NULL);
+      if(execl("/bin/bash", "", (char*)NULL) == -1) {
+	fprintf(stderr, strerror(errno));
+	exit(1);
+      }
     }
     else {
       close(pipefd_in[0]);
@@ -90,35 +106,33 @@ int main(int argc, char** argv) {
       int status;
       while(1) {
 	int ret = poll(fds, 2, 0);
+	if(ret == -1) {
+	  fprintf(stderr, strerror(errno));
+	  exit(1);
+	}
 	if(ret > 0) {
 	  if(fds[0].revents & POLLIN) { // read input from the keyboard
 	    n = read(fds[0].fd, buffer, BUFSIZE);
 	    for(int i = 0; i < n; i++) {
 	      if(buffer[i] == '\r' || buffer[i] == '\n') {
 		write(STDOUT_FILENO, "\r\n", 2);
-		if(write(pipefd_in[1], "\n", 1) == SIGPIPE) {
-		  exit_shell = 1;
-		}
+		write(pipefd_in[1], "\n", 1);
 	      }
 	      else if(buffer[i] == 0x03) {
-		//if(write(pipefd_in[1], &buffer[i], 1) == SIGPIPE) {
-		//  exit_shell = 1;
-		//}
+		write(pipefd_in[1], &buffer[i], 1);
 		write(STDOUT_FILENO, "^C", 2);
-		kill(c_pid, SIGINT);
+		if(kill(c_pid, SIGINT) == -1) {
+		  fprintf(stderr, strerror(errno));
+		  exit(1);
+		}
 	      }
 	      else if(buffer[i] == 0x04) {
-		//if(write(pipefd_in[1], &buffer[i], 1) == SIGPIPE) {
-		//  exit_shell = 1;
-		//}
 		close(pipefd_in[1]);
 		write(STDOUT_FILENO, "^D", 2);
 	      }
 	      else {
 		write(STDOUT_FILENO, &buffer[i], 1);
-		if(write(pipefd_in[1], &buffer[i], 1) == SIGPIPE) {
-		  exit_shell = 1;
-		}
+		write(pipefd_in[1], &buffer[i], 1);
 	      }
 	    }
 	  }
@@ -128,18 +142,14 @@ int main(int argc, char** argv) {
 	      if(buffer[i] == '\n') {
 		write(STDOUT_FILENO, "\r\n", 2);
 	      }
-	      else if(buffer[i] == 0x04 || (fds[1].revents & POLLERR)) {
-		exit_shell = 1;
-	      }
 	      else {
 		write(STDOUT_FILENO, &buffer[i], 1);
 	      }
 	    }
 	  }
-	  //if((fds[1].revents & POLLHUP) || (fds[1].revents & POLLERR)) {
-	  //  reset_input_mode();
-	  //  exit(0);
-	  //}
+	  if((fds[1].revents & POLLHUP) || (fds[1].revents & POLLERR)) {
+	    exit_shell = 1;
+	  }
 	}
 	
 	if(exit_shell) {
